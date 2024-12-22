62e98bafd259f545c40c2c836e2a23508f0412c2
sohamjagrit <sohamjagrit@gmail.com>
2024-12-22 08:17:05 -0600

    Creates the project structure and installed all the libraries

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'NutriBot',
+    version= '0.0.0',
+    author= 'Soham Jagrit',
+    author_email= 'sohamjagrit@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
